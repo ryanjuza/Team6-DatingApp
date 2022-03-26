5b52694848f503315ee3697f13112a978f28f46e
Reza <reebrah@gmail.com>
2022-03-26 12:11:02 -0400

    Created post request for user login/authorization

--- app.js
+++ app.js
@@ -97,11 +97,6 @@ const Mprofile = mongoose.model('Mprofile', mSchema);
 
 
 
-
-
-
-
-
 const mlikeSchema = new mongoose.Schema({
     _id: {type: mongoose.Schema.Types.ObjectId, ref: 'mSchema'},
     women: [{wid: {type: mongoose.Schema.Types.ObjectId, ref: 'wSchema'}, seen: String, rate: Number}]
@@ -136,6 +131,8 @@ app.get("/interests", function(req, res){
     res.render("interests");
 });
 
+
+
 app.post("/register", function(req, res){
     const fn = req.body.fname;
     const gn = req.body.gender;
@@ -145,50 +142,49 @@ app.post("/register", function(req, res){
     const loc = req.body.location
 
 
-
-    Profile.register({username: req.body.username, name: fn, gender: gn}, req.body.password, function(err, profs){
+    Profile.register({username: req.body.username, name: fn, gender: gn, age: age, location: loc, gInterests: genInter}, req.body.password, function(err, profs){
         if (err){
             console.log(err);
         } else {
             const gen = profs._id;
-            passport.authenticate("local")(req, res, function(){
+            passport.authenticate("local");
+            if (gn == "male"){
+                const newMp = new Mprofile({
+                    _id: gen,
+                    name: fn,
+                    username: un,
+                    gender: gn,
+                    age: age,
+                    location: loc,
+                    gInterests: genInter
+                });
+                newMp.save();
+                const startLike = new Like({
+                    _id: gen
+                });
+                startLike.save();
 
-                if (gn == "male"){
-                    const newMp = new Mprofile({
-                        _id: gen,
-                        name: fn,
-                        username: un,
-                        gender: gn,
-                        age: age,
-                        location: loc,
-                        gInterests: genInter
-                    });
-                    newMp.save();
-                    const startLike = new Like({
-                        _id: gen
-                    });
-                    startLike.save();
-    
-    
-                }
-                
-                if(gn == "female"){
-                    const newWp = new Wprofile({
-                        _id: gen,
-                        name: fn,
-                        username: un,
-                        gender: gn,
-                        age: age,
-                        location: loc,
-                        gInterests: genInter
-                    });
-                    newWp.save();
-                    const startFlike = new Flike({
-                        _id: gen
-                    });
-                    startFlike.save();
-          
-                }
+            }
+            
+            if(gn == "female"){
+                const newWp = new Wprofile({
+                    _id: gen,
+                    name: fn,
+                    username: un,
+                    gender: gn,
+                    age: age,
+                    location: loc,
+                    ginterests: genInter
+                });
+                newWp.save();
+                const startFlike = new Flike({
+                    _id: gen
+                });
+                startFlike.save();
+      
+            }
+
+            passport.authenticate("local")(req, res, function(){
 
                 res.redirect("/interests");
             });
@@ -198,7 +194,27 @@ app.post("/register", function(req, res){
         
         
     });
+
+
 });
+
+
+app.post("/logins", function(req, res){
+    const newProfs = new Profile({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    req.login(newProfs, function(err){
+        if (err) {
+            console.log(err);
+        } else {
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/home");
+            });
+        }
+    });
+})
 
 app.listen(3000, function(){
     console.log("Server started successfully");
